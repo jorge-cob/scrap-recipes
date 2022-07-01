faee778596946ee2e2e90f178b799c872ffb6934
jorge-cob <jorge.cobreros@protonmail.com>
2022-07-01 19:29:33 +0200

    color patterns

---
+++ assets/theme/constants/Colors.ts
@@ -0,0 +1,113 @@
+export const STRONG_BLACK = '#000000';
+export const BLACK = '#333333';
+export const WHITE = '#FFF';
+
+export const STRONG_ORANGE = '#FB3300';
+export const ORANGE = '#F5A282';
+export const LIGHT_ORANGE = '#FDF2EE';
+
+export const STRONG_DEEP_BLUE = '#1827B4';
+export const DEEP_BLUE = '#8990DA';
+export const LIGHT_DEEP_BLUE = '#F2F2FF';
+
+export const STRONG_PINK = '#ED4B8E';
+export const PINK = '#ECB3D0';
+export const LIGHT_PINK = '#FFF2F9';
+
+export const STRONG_GREEN = '#0E9888';
+export const GREEN = '#6CC8BE';
+export const LIGHT_GREEN = '#E7FFFC';
+
+export const STRONG_BLUE = '#4CDAFD';
+export const BLUE = '##9DE3FD';
+export const LIGHT_BLUE = '#DDEBFD';
+
+export const STRONG_LIME = '#CDED4D';
+export const LIME = '#DDED9E';
+export const LIGHT_LIME = '#EAEDDE';
+
+export const STRONG_PURPLE = '#C34CED';
+export const PURPLE = '#D89DED';
+export const LIGHT_PURPLE = '#E9DDED';
+
+export const STRONG_PURE_BLUE = '#009AFE';
+export const PURE_BLUE = '#81B8DD';
+export const LIGHT_PURE_BLUE = '#EDF8FG';
+
+export const STRONG_RED = '#E8525F';
+export const RED = '#E9A4A8';
+export const LIGHT_RED = '#FFEFF1';
+
+export const STRONG_YELLOW = '#E8C71B';
+export const YELLOW = '#E9DC98';
+export const LIGHT_YELLOW = '#FFFBE8';
+
+export const STRONG_SUCCESS_GREEN = '#0FA856';
+export const SUCCESS_GREEN = '#8FDBB3';
+export const LIGHT_SUCCESS_GREEN = '#E8FFF4';
+
+export const GRAY_9 = '#E5E5E4';
+export const GRAY_6 = '#666665';
+export const GRAY_5 = '#999998';
+export const GRAY_4 = '#B8B8B7';
+export const GRAY_3 = '#CCCCCD';
+export const GRAY_2 = '#E6E6E5';
+export const GRAY_1 = '#EFEFEE';
+
+export default {
+  STRONG_BLACK,
+  BLACK,
+  WHITE,
+
+  ORANGE,
+  STRONG_ORANGE,
+  LIGHT_ORANGE,
+
+  STRONG_BLUE,
+  BLUE,
+  LIGHT_BLUE,
+
+  STRONG_PINK,
+  PINK,
+  LIGHT_PINK,
+
+  STRONG_GREEN,
+  GREEN,
+  LIGHT_GREEN,
+
+  STRONG_PURE_BLUE,
+  PURE_BLUE,
+  LIGHT_PURE_BLUE,
+
+  RED,
+  STRONG_RED,
+  LIGHT_RED,
+
+  YELLOW,
+  STRONG_YELLOW,
+  LIGHT_YELLOW,
+
+  SUCCESS_GREEN,
+  STRONG_SUCCESS_GREEN,
+  LIGHT_SUCCESS_GREEN,
+
+  GRAY_9,
+  GRAY_6,
+  GRAY_5,
+  GRAY_4,
+  GRAY_3,
+  GRAY_2,
+  GRAY_1,
+
+  STRONG_DEEP_BLUE,
+  DEEP_BLUE,
+  LIGHT_DEEP_BLUE,
+
+  STRONG_LIME,
+  LIME,
+  LIGHT_LIME,
+
+  STRONG_PURPLE,
+  PURPLE,
+  LIGHT_PURPLE
+};
