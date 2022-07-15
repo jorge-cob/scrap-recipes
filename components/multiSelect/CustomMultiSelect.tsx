7cfa8bf60b42390a0ffa6c48664b932116991b20
jorge-cob <jorge.cobreros@protonmail.com>
2022-07-15 16:37:56 +0200

    multi select styling

--- components/multiSelect/CustomMultiSelect.tsx
+++ components/multiSelect/CustomMultiSelect.tsx
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 import SubmitButton from '../button/button';
+import { PURE_BLUE } from '../../assets/theme/constants/Colors';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: PURE_BLUE,
+    borderRadius: 15,
+    alignSelf: 'center',
+    marginBottom: 30,
+    marginHorizontal: 40,
+    width: 1250
+  },
+});
 
 function CustomMultiSelect() {
   const [items, setItems] = useState([
@@ -43,12 +55,13 @@ function CustomMultiSelect() {
 
   function AddItemComponent(searchText: string) {
     return (
-      <SubmitButton handleOnPress={() => addItemToList(searchText)} title={`Add ${searchText} as ingredient`} />
+      <SubmitButton handleOnPress={() => addItemToList(searchText)} title={`Add ${searchText}`} />
+      //insert icon here
     );
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <SectionedMultiSelect
         items={items}
         IconRenderer={Icon}
